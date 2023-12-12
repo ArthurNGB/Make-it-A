f28ecdbc976fd59224a19b7f32c1697df9f81138
ArthurNGB <135652200+ArthurNGB@users.noreply.github.com>
2023-12-12 02:03:31 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,1 @@
-$(document).ready(function(data){
-    $.getJSON("main.json", function(data){
-        console.log(data)
 
-        $('.a-box-item-1').html(data.AtrNome);
-        $('.a-box-item-2-1').html(data.AtrData);
-        $('.sd-box-item-1').html(data.SdNome);
-        $('.h-box-item-1').html(data.HjNome);
-        $('.h-box-item-2').html(data.HjData);
-        $('.em-box-item-1').html(data.EmNome);
-        $('.em-box-item-2').html(data.EmData);
-
-    }).fail(function(){
-        console.log("Revise seu codigo")
-    })
-})
